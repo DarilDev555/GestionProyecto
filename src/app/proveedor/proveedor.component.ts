1682a141ec88b859aa21d0aca5dff76e22fb46c5
Daril <oneandrey555@outlook.com>
2023-11-22 01:08:15 -0600

    proveedores

--- src/app/proveedor/proveedor.component.ts
+++ src/app/proveedor/proveedor.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog'; 
+import { Proveedor } from '../modelos/proveedor';
 
 
 
@@ -51,7 +52,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 ];
 
 @Component({
-  selector: 'app-dashboard',
+  selector: 'proveedor',
   templateUrl: './proveedor.component.html',
   styleUrls: ['./proveedor.component.css'],
 })
@@ -62,13 +63,29 @@ export class ProveedorComponent {
     'telefono',
     'correo',
     'direccion',
+    'accion',
   ];
   dataSource = ELEMENT_DATA;
   isDrawerOpened = false;
   name = '';
   userName = '';
   imag = '';
-  constructor(private router: Router, public dialog: MatDialog) {}
+  buscarTexto: string = '';
+  proveedoresTotales: Proveedor[] = [];
+  proveedorSelect: Proveedor[] = [];
+  constructor(private router: Router, public dialog: MatDialog) {
+
+    this.proveedoresTotales = ELEMENT_DATA.map((element) => {
+      return {
+        id: element.id,
+        name: element.name,
+        telefono: element.telefono,
+        correo: element.correo,
+        direccion: element.direccion,
+      };
+    });
+
+  }
   toggleDrawer() {
     this.isDrawerOpened = !this.isDrawerOpened;
   }
@@ -80,5 +97,92 @@ export class ProveedorComponent {
   toLogin() {
     this.router.navigate(['']);
   }
+
+  resetCantidadInput() {
+  }
+
+  proveedorDelete(element: any) {
+    const index = this.proveedoresTotales.findIndex(
+      (proveedor: Proveedor) => proveedor.id ===element.id
+    );
+
+      this.proveedoresTotales.splice(index, 1);
+
+
+    this.dataSource = this.proveedoresTotales.map((proveedor: Proveedor) => {
+      return {
+        id: proveedor.id,
+        name: proveedor.name,
+        telefono: proveedor.telefono,
+        correo: proveedor.correo,
+        direccion: proveedor.direccion,
+      };
+    });
+  }
+
+  proveedorAdd() {
+  }
+
+  proveedorEdit() {
+  }
+
+  proveedorBuscar() {    
+    const buscarTexto = this.buscarTexto.trim(); // Elimina espacios en blanco alrededor
+    const id = parseInt(buscarTexto);
+
+    console.log('texto:', buscarTexto);
+    console.log('totales:', this.proveedoresTotales);
+
+    if (!isNaN(id)) {
+      // Si la conversión a número es exitosa, busca por ID
+      const proveedorExixtente = this.proveedoresTotales.find(
+        (proveedor) => proveedor.id === id
+      );
+
+      if (proveedorExixtente) {
+        this.elementDataToProveedor(proveedorExixtente);
+        console.log('Encontrado (por ID):', proveedorExixtente);
+      } else {
+        console.log('Producto no encontrado (por ID)', proveedorExixtente);
+      }
+    } else {
+      // Si no es un número, busca por nombre
+      const nombre = buscarTexto.toLowerCase(); // Convierte el texto de búsqueda a minúsculas
+
+      const proveedorExixtente = this.proveedoresTotales.find(
+        (proveedor) => this.proveedorAdd.name.toLowerCase() === nombre
+      );
+
+      if (proveedorExixtente) {
+        console.log('Encontrado (por nombre):', proveedorExixtente);
+        this.elementDataToProveedor(proveedorExixtente);
+      } else {
+        console.log('Producto no encontrado (por nombre)');
+      }
+    }
+  }
+
+  elementDataToProveedor(proveedor: Proveedor) {
+    // Crea una nueva matriz de un solo elemento con el producto
+    const newElementData: PeriodicElement[] = [
+      {
+        id: proveedor.id,
+        name: proveedor.name,
+        telefono: proveedor.telefono,
+        correo: proveedor.correo,
+        direccion: proveedor.direccion,
+      },
+    ];
+
+    // Actualiza dataSource para reflejar los cambios
+    this.dataSource = newElementData;
+    console.log('elementdata:', ELEMENT_DATA);
+    console.log('datasource:', this.dataSource);
+  }
+
+  limpiarInput() {
+    this.buscarTexto = '';
+  }
+
 
 }
